d885df7534a578759107032b241d865f9fdb200f
bobnat1 <natehuser@gmail.com>
2024-04-02 21:54:45 -0700

    completed part 2

--- components/WeatherCard.jsx
+++ components/WeatherCard.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import sunny from "../assets/Sunny.svg"
+import sunny from "../assets/Sunny.svg";
+import rainy from "../assets/Rainy.svg";
+import partlyCloudy from "../assets/PartlyCloudy.svg";
+import cloudy from "../assets/Cloudy.svg";
 
 
 function WeatherCard(props) {
   return (
     <div className = "card">
         <div className = "img-container">
-            <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
+            <img className="card-img-top" src = {props.city.forecast === "Sunny" ? sunny : props.city.forecast === "Rainy" ? rainy : props.city.forecast === 'Cloudy' ? cloudy : partlyCloudy} alt="Card image cap" id = "icon"/>
         </div>
         <div class="card-body">
             <h3 className="card-title">{props.city.city}</h3>
